ecf3e0ba593c501d25c34cac8d742788d92821c6
safi7 <qandagha.safi17@gmail.com>
2018-08-06 15:20:05 +0800

    test

--- app/src/main/java/madcatworld/com/my_diary/MainActivity.java
+++ app/src/main/java/madcatworld/com/my_diary/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-
+//testing gethub 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener( ) {
 
